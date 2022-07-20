f996e91ae118947f1f1384d35f4eac92f90e5605
Papillon6814 <so1rou.k@gmail.com>
2022-07-20 16:02:33 +0900

    chore: publish

--- mix.exs
+++ mix.exs
@@ -6,7 +6,9 @@ defmodule ExWhatKit.MixProject do
       app: :ex_what_kit,
       version: "0.1.0",
       elixir: "~> 1.13",
+      description: "Whatsapp Automation tool",
       start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps()
     ]
   end
@@ -23,6 +25,15 @@ defmodule ExWhatKit.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.22.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Papillon6814"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/Papillon6814/ex_what_kit"}
     ]
   end
 end
